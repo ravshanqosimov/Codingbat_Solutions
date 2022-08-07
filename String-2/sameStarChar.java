f561321dd42592123bafb3846b158d7dbd508032
Ravshan <74915310+ravshanqosimov@users.noreply.github.com>
2022-08-07 17:08:04 +0500

    Update sameStarChar.java

--- String-2/sameStarChar.java
+++ String-2/sameStarChar.java
@@ -5,6 +5,17 @@ sameStarChar("xy*yzz") → true
 sameStarChar("xy*zzz") → false
 sameStarChar("*xa*az") → true
 */
+
+// method 1
+// public boolean sameStarChar(String str) {
+//   boolean eq=true;
+//   for (int i=1;i<str.length()-1;i++){
+//     if(str.charAt(i)=='*'&&str.charAt(i-1)!=str.charAt(i+1)){eq= false;break;}
+//   }
+//   return eq;
+// }
+
+
 public boolean sameStarChar(String str) {
   int z = -1;
   for(int i=1; i < str.length()-1; i++)
